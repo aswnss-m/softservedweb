0094b12c400dde4e0bee55af3e711e4111658878
Aniz <20am014@gmail.com>
2023-04-10 07:51:44 +0530

    hovering effects added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,21 +5,22 @@ function Footer() {
   return (
     <div className="footerSection">
       <div className="footerPart1">
-        <h3>Disclaimer</h3>
+      <a href=""> <h3>Disclaimer</h3></a>
       </div>
       <div className="footerPart2">
-        <h3>softservedweb@gmail.com</h3>
+      <a href=""><h3>softservedweb@gmail.com</h3></a>
       </div>
       <div className="footerPart3">
-        <h3 className="part31">Blog</h3>
-        <h3 className="part32">Our Works</h3>
-        <h3 className="part33">About</h3>
+        <a href=""></a>
+        <a href=""><h3 className="bio part31">Blog</h3></a>
+        <a href=""> <h3 className="bio part32">Our Works</h3></a>
+        <a href=""> <h3 className="bio part33">About</h3></a>
       </div>
       <div className="footerPart4">
-        <h3 className="socials part41">Twitter</h3>
-        <h3 className="socials part42">Instgram</h3>
-        <h3 className="socials part43">Github</h3>
-        <h3 className="socials part44">Instgram</h3>
+      <a href=""> <h3 className="socials part41">Twitter</h3></a>
+      <a href=""> <h3 className="socials part42">Instgram</h3></a>
+      <a href=""> <h3 className="socials part43">Github</h3></a>
+      <a href=""> <h3 className="socials part44">Instgram</h3></a>
       </div>
     </div>
   );
