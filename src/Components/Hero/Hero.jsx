09a3d697fca80a71591cde45d32d378aac2e5c9e
Aniz <20am014@gmail.com>
2023-04-14 16:13:32 +0530

    Added hero page animations

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,6 +4,37 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="HeroSection">
+      <div className="otherDiv">
+        <div className="scene">
+          <div className="wrapper">
+            <ul className="ball">
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+              <li className="ring"></li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div className="container">
         <div className="TitleContainer">
           <div className="titleMain">
@@ -11,19 +42,80 @@ function Hero() {
             <h1 className="title Serve">Served</h1>
             <h1 className="title Web">Web</h1>
           </div>
-
-          <div className="otherDiv">
-            <h1>Yet to be decided ! </h1>
-          </div>
         </div>
 
         <div className="ScrollableTile">
           <div className="marquee">
-           <a href=""> <h3 className="info">we make web based solutions</h3> </a>
-           <a href=""> <h3 className="info">get cusotm solutions</h3></a>
-           <a href=""> <h3 className="info">we make AR based products</h3></a>
-           <a href=""> <h3 className="info">contact us</h3></a>
-           <a href=""> <h3 className="info">made up of pro well experienced developers</h3></a>
+            <a href="">
+              {" "}
+              <h3 className="info">we make web based solutions</h3>{" "}
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">get cusotm solutions</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">we make AR based products</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">contact us</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">
+                made up of pro well experienced developers
+              </h3>
+            </a>
+          </div>
+          <div className="marquee">
+            <a href="">
+              {" "}
+              <h3 className="info">we make web based solutions</h3>{" "}
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">get cusotm solutions</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">we make AR based products</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">contact us</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">
+                made up of pro well experienced developers
+              </h3>
+            </a>
+          </div>
+          <div className="marquee">
+            <a href="">
+              {" "}
+              <h3 className="info">we make web based solutions</h3>{" "}
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">get cusotm solutions</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">we make AR based products</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">contact us</h3>
+            </a>
+            <a href="">
+              {" "}
+              <h3 className="info">
+                made up of pro well experienced developers
+              </h3>
+            </a>
           </div>
         </div>
       </div>
