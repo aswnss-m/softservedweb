06f0f706d830f88004fb31e40c64029208cc5380
Aniz <20am014@gmail.com>
2023-04-20 01:12:29 +0530

    animation added for the front page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,40 +1,50 @@
 import React from "react";
 import "./Hero.css";
+import {
+  card101,
+  card111,
+  card202,
+  card222,
+  card303,
+  card333,
+} from "../../assets/export";
 
 function Hero() {
   return (
     <div className="HeroSection">
-      <div className="otherDiv">
-        <div className="scene">
-          <div className="wrapper">
-            <ul className="ball">
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-              <li className="ring"></li>
-            </ul>
+      {/* <div className="otherDiv">
+          <div className="scene">
+            <div className="wrapper">
+              <ul className="ball">
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+                <li className="ring"></li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </div>
+        </div> */}
+
+
       <div className="container">
         <div className="TitleContainer">
           <div className="titleMain">
@@ -42,6 +52,57 @@ function Hero() {
             <h1 className="title Serve">Served</h1>
             <h1 className="title Web">Web</h1>
           </div>
+
+        <div className="cardContainer">
+
+        
+        <div className="scrollableCardTop">
+        <div className="topScroll">
+          <img src={card101} alt="" />
+          <img src={card111} alt="" />
+          <img src={card202} alt="" />
+          <img src={card222} alt="" />
+          <img src={card303} alt="" />
+          <img src={card333} alt="" />
+        </div>
+        <div className="topScroll">
+          <img src={card101} alt="" />
+          <img src={card111} alt="" />
+          <img src={card202} alt="" />
+          <img src={card222} alt="" />
+          <img src={card303} alt="" />
+          <img src={card333} alt="" />  
+          <img src={card333} alt="" />  
+
+
+        </div>
+      </div>
+        <div className="scrollableCardBottom">
+        <div className="bottomScroll">
+          <img src={card101} alt="" />
+          <img src={card111} alt="" />
+          <img src={card202} alt="" />
+          <img src={card222} alt="" />
+          <img src={card303} alt="" />
+          <img src={card333} alt="" />
+        </div>
+        <div className="bottomScroll">
+          <img src={card101} alt="" />
+          <img src={card111} alt="" />
+          <img src={card202} alt="" />
+          <img src={card222} alt="" />
+          <img src={card303} alt="" />
+          <img src={card333} alt="" />
+          <img src={card101} alt="" />
+          {/* <img src={card111} alt="" /> */}
+
+          
+   
+      
+        </div>
+      </div>
+      </div>
+
         </div>
 
         <div className="ScrollableTile">
--- src/assets/export.js
+++ src/assets/export.js
@@ -6,6 +6,12 @@ import boxImage3 from './images/boxImage3.png'
 import boxImage4 from './images/boxImage4.png'
 import boxImage5 from './images/boxImage5.png'
 import boxImage6 from './images/boxImage6.png'
+import card101 from './heroImages/101.png'
+import card111 from './heroImages/111.png'
+import card202 from './heroImages/202.png'
+import card222 from './heroImages/222.png'
+import card303 from './heroImages/303.png'
+import card333 from './heroImages/333.png'
 
 export {
     tempPic,
@@ -15,5 +21,12 @@ export {
     boxImage3,
     boxImage4,
     boxImage5,
-    boxImage6
+    boxImage6,
+
+    card101,
+    card111,
+    card202,
+    card222,
+    card303,
+    card333,
 }
