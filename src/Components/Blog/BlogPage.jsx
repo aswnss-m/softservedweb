f14065cfba9ae5440edba1fd2870e8d89d2f3ccf
aswnss-m <aswnss.m@gmail.com>
2023-04-30 18:17:34 +0530

    Blog main page almost done

--- src/Components/Blog/BlogPage.jsx
+++ src/Components/Blog/BlogPage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import "./Blog.css";
+import { Blogs } from '../../assets/Blogs';
 
-function BlogPage() {
+function BlogPage(props) {
   return (
-    <div>IndividualBlog</div>
+    <div className='blogPage'>
+      <div className="blogHeader">
+        <h4><span className='blogHeaderLogo'>Soft<br />Served<br />Web</span></h4>
+        <span className='blogHeaderTitle'>Blogs</span>
+        <p className='blogHeaderDescp'>Every now and then, a new technology comes we do a project and we will publish a blog regarding that project or research, so that it becomes easier for you to understand</p>
+      </div>
+      <div className="blogContent">
+        <div className="blogPageTitle">
+          {
+            Blogs[0].Title
+            //props.Title
+          }
+        </div>
+        <div className="blogPageContent">
+          {
+            Blogs[1].Content
+            //props.Content
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
